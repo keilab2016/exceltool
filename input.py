14e237a8ff6b46de882fb6ac661f89b8909181e6
Kei Itou <kei2006@fun.ac.jp>
2021-02-19 15:27:16 +0900

    使わなくなったx_list_dataも削除

--- input.py
+++ input.py
@@ -2,19 +2,19 @@ import glob
 #import label_year 
 year_list = []
 term_list = ["年度前期_回答データ", "年度後期_回答データ"]
-x_list_data = []
+#x_list_data = []
 def make_year_list(admission_year):
     year_list.clear()
-    x_list_data.clear()
+    #x_list_data.clear()
     year = int(admission_year)
     for i in range(8):
         year_list.append(year)
         if((i+1) % 2 == 0):
             year = year + 1
-            x_list_data.append(str(year_list[i]) + "年後期")
-        else:
-            x_list_data.append(str(year_list[i]) + "年前期")
-    x_list_data.append(str(year) + "年卒業時")
+            #x_list_data.append(str(year_list[i]) + "年後期")
+        #else:
+        #    x_list_data.append(str(year_list[i]) + "年前期")
+    #x_list_data.append(str(year) + "年卒業時")
     #label_year.x_list = x_list_data
 
 
