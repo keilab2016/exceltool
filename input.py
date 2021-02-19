fd42f15d199b087225a86e1e6428c767fa7d63e7
Kei Itou <kei2006@fun.ac.jp>
2021-02-19 15:48:15 +0900

    卒業時データは最後になるように調整

--- input.py
+++ input.py
@@ -4,7 +4,11 @@ input_path='Templete/'
 
 def input_from_excel(): #入学年を取得し、読み込むデータを決定
     files=[]
+    last_files=[]
     for f in os.listdir(input_path):
-        files.append(input_path + f)
-    print(files)
+        if '卒業' in f:
+            last_files.append(input_path + f)
+        else:
+            files.append(input_path + f)
+    files.append(last_files[len(last_files)-1])
     return(files)
