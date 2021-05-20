81a059ff6d12ae2267d384aacb70b46accc00ca8
Kei Itou <kei2006@fun.ac.jp>
2021-05-20 19:57:48 +0900

    卒業時データを読み込まない場合の対応

--- input.py
+++ input.py
@@ -10,5 +10,6 @@ def input_from_excel(): #入学年を取得し、読み込むデータを決定
             last_files.append(input_path + f)
         else:
             files.append(input_path + f)
-    files.append(last_files[len(last_files)-1])
+    if len(last_files) > 0:
+        files.append(last_files[len(last_files)-1])
     return(files)
