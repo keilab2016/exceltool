8994bb116a2d5b54599ebc8d0cbad2d67e93ad04
伊藤恵研究室 <member@keilab-macmini2018.local>
2021-11-07 10:00:50 +0900

    Macの場合はpythonの代わりにpython3を実行

--- all2.sh
+++ all2.sh
@@ -3,17 +3,22 @@ STUDENTLIST=student-list.txt
 if [ $# -gt 0 -a -f $1 ]; then
 	STUDENTLIST=$1
 fi
+if [ "$(uname)" == "Darwin" ]; then
+        CMD=python3
+else
+        CMD=python
+fi
 j=0
 list=()
 for i in `cat $STUDENTLIST`; do
 	list=(${list[@]} $i)
 	let j++
-	if [ $j == 15 ]; then
-		python excel_job.py ${list[@]}
+	if [ $j == 40 ]; then
+		$CMD excel_job.py ${list[@]}
 		j=0
 		list=()
 	fi
 done
 if [ $j != 0 ]; then
-	python excel_job.py ${list[@]}
+	$CMD excel_job.py ${list[@]}
 fi
