ebab4977810df21d9598d25fd76340cc3eddbbfd
伊藤恵研究室 <member@keilab-macmini2018.local>
2021-11-07 14:34:02 +0900

    アンケート回答Excelファイルのファイル形式エラー

--- excel_job.py
+++ excel_job.py
@@ -10,6 +10,7 @@ import openpyxl
 import os
 import re
 import copy
+import xlrd
 
 #ディレクトリが存在していなければ作成
 if(os.path.exists('image') == False):
@@ -34,13 +35,19 @@ for path in input_from_excel():
     if fname.endswith('.xlsx'):
         # Moodle
         flist.append(fname.replace('.xlsx',''))
-        df=pd.read_excel('%s' % path)
-        print('Moodle',path)
+        try:
+            df=pd.read_excel('%s' % path)
+            print('Moodle',path)
+        except xlrd.biffh.XLRDError:
+            print('Moodle',path,'Unsupported or Corrupt file')
     else:
         # manaba
         flist.append(fname.replace('.xls',''))
-        df=pd.read_excel('%s' % path, header=5)
-        print('manaba',path)
+        try:
+            df=pd.read_excel('%s' % path, header=5)
+            print('manaba',path)
+        except xlrd.biffh.XLRDError:
+            print('manaba',path,'Unsupported or Corrupt file')
     try:
         df1=df[section.sectionList] #section.pyから列名のリストを取得
     except KeyError:
